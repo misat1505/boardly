75c497d0f554e03a771f2af2972d033177c2648b
misat1505 <misat2002@gmail.com>
2025-07-16 15:04:03 +0200

    teams display on dashboard

--- frontend/src/app/dashboard/page.tsx
+++ frontend/src/app/dashboard/page.tsx
@@ -1,5 +1,26 @@
-const DashboardPage = () => {
-  return <div>dashboard</div>;
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+
+const DashboardPage = async () => {
+  const cookiesStore = await cookies();
+  const accessToken = cookiesStore.get("accessToken")?.value;
+
+  const teams = await fetch("http://localhost:8080/teams", {
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+    },
+  }).then((res) => {
+    if (!res.ok) redirect("/login");
+    return res.json();
+  });
+
+  return (
+    <div>
+      {teams.map((team: any) => (
+        <div key={team.id}>{team.name}</div>
+      ))}
+    </div>
+  );
 };
 
 export default DashboardPage;
