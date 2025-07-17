e776bef0be47ba8b3d9be4157446769d4336d0c1
misat1505 <misat2002@gmail.com>
2025-07-17 15:55:27 +0200

    user dropdown menu in dashboard navbar

--- frontend/src/components/dashboard/DashboardNavbar.tsx
+++ frontend/src/components/dashboard/DashboardNavbar.tsx
@@ -1,6 +1,13 @@
 import { getCurrentUser } from "@/actions/user/getCurrentUser";
 import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
 import { ThemeSwitch } from "../ui/theme-switch";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "../ui/dropdown-menu";
+import { Button } from "../ui/button";
 
 const DashboardNavbar = async () => {
   const user = await getCurrentUser();
@@ -10,10 +17,30 @@ const DashboardNavbar = async () => {
       <h2 className="font-bold text-muted-foreground">Hi {user.givenName}!</h2>
       <div className="flex justify-center items-center gap-x-2">
         <ThemeSwitch />
-        <Avatar>
-          <AvatarImage src={user.imageUrl} alt={user.username} />
-          <AvatarFallback>{user.username.toUpperCase()[0]}</AvatarFallback>
-        </Avatar>
+
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button
+              variant="outline"
+              size="icon"
+              className="hover:cursor-pointer"
+            >
+              <Avatar>
+                <AvatarImage src={user.imageUrl} alt={user.username} />
+                <AvatarFallback>
+                  {user.username.toUpperCase()[0]}
+                </AvatarFallback>
+              </Avatar>
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end" className="w-fit min-w-40">
+            <h2 className="font-bold text-center px-1 pt-1">{user.username}</h2>
+            <h2 className="text-xs text-center px-1 pb-1">{user.email}</h2>
+            <DropdownMenuItem>Preferences</DropdownMenuItem>
+            <DropdownMenuItem>Go Pro</DropdownMenuItem>
+            <DropdownMenuItem>Logout</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
       </div>
     </nav>
   );
