f81605af6542d17a72464d17af6e85cdf9d2d0f3
misat1505 <misat2002@gmail.com>
2025-07-25 13:25:32 +0200

    created websocket service

---
+++ websocket/src/events.ts
@@ -0,0 +1,6 @@
+export enum Events {
+  JOIN_ROOM = "join-room",
+  LEAVE_ROOM = "leave-room",
+  SEND_BOARD = "send-board",
+  RECEIVE_ROOM = "receive-room",
+}
---
+++ websocket/src/index.ts
@@ -0,0 +1,33 @@
+import { createServer } from "http";
+import { Server as SocketIOServer } from "socket.io";
+import dotenv from "dotenv";
+import { Events } from "./events";
+
+dotenv.config();
+
+const httpServer = createServer();
+
+const io = new SocketIOServer(httpServer, {
+  cors: {
+    origin: process.env.FRONTEND_UL,
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  socket.on(Events.SEND_BOARD, (board, room) => {
+    socket.to(room).emit(Events.RECEIVE_ROOM, board);
+  });
+
+  socket.on(Events.JOIN_ROOM, async (room) => {
+    socket.join(room);
+  });
+
+  socket.on(Events.LEAVE_ROOM, (room) => {
+    socket.leave(room);
+  });
+});
+
+httpServer.listen(process.env.PORT, () => {
+  console.log(`Socket.IO server running on port ${process.env.PORT}`);
+});
