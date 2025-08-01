fe5d3bfa2f8c4779aa0aeb6893333bfd041e14fb
misat1505 <misat2002@gmail.com>
2025-08-01 18:23:39 +0200

    refactored TeamController to BoardController

---
+++ backend/src/main/java/com/example/backend/controllers/BoardController.java
@@ -0,0 +1,52 @@
+package com.example.backend.controllers;
+
+import com.example.backend.application.services.BoardService;
+import com.example.backend.domain.dtos.CreateBoardDTO;
+import com.example.backend.domain.dtos.UpdateBoardDTO;
+import com.example.backend.domain.entities.Board;
+import com.example.backend.exceptions.boards.BoardNotFoundException;
+import com.example.backend.exceptions.boards.TooManyBoardsException;
+import com.example.backend.exceptions.teams.TeamNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+import java.util.Set;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/teams")
+public class BoardController {
+    private final BoardService boardService;
+
+    public BoardController(BoardService boardService) {
+        this.boardService = boardService;
+    }
+
+    @GetMapping("/{teamId}/boards")
+    public ResponseEntity<Set<Board>> getTeamBoards(@PathVariable UUID teamId) {
+        Set<Board> boards = boardService.getTeamBoards(teamId);
+        return ResponseEntity.ok(boards);
+    }
+
+    @GetMapping("/boards/{boardId}")
+    public ResponseEntity<Board> getBoardById(@PathVariable UUID boardId) {
+        Optional<Board> board = boardService.getBoardById(boardId);
+        return board.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+
+    }
+
+    @PutMapping("/boards/{boardId}")
+    public ResponseEntity<Board> updateBoard(@PathVariable UUID boardId, @RequestBody UpdateBoardDTO updateBoardDTO) throws BoardNotFoundException {
+        Board board = boardService.updateBoard(boardId, updateBoardDTO);
+        return ResponseEntity.ok(board);
+    }
+
+    @PostMapping("/{teamId}/boards")
+    public ResponseEntity<Board> createBoard(@PathVariable UUID teamId, @RequestBody CreateBoardDTO dto) throws TooManyBoardsException, TeamNotFoundException {
+        dto.setTeamId(teamId);
+        Board board = boardService.createBoard(dto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(board);
+    }
+}
--- backend/src/main/java/com/example/backend/controllers/TeamController.java
+++ backend/src/main/java/com/example/backend/controllers/TeamController.java
@@ -2,15 +2,10 @@ package com.example.backend.controllers;
 
 import com.example.backend.application.services.BoardService;
 import com.example.backend.application.services.TeamService;
-import com.example.backend.domain.dtos.CreateBoardDTO;
 import com.example.backend.domain.dtos.CreateTeamDTO;
 import com.example.backend.domain.dtos.InviteUserToTeamDTO;
-import com.example.backend.domain.dtos.UpdateBoardDTO;
-import com.example.backend.domain.entities.Board;
 import com.example.backend.domain.entities.Team;
 import com.example.backend.domain.entities.User;
-import com.example.backend.exceptions.boards.BoardNotFoundException;
-import com.example.backend.exceptions.boards.TooManyBoardsException;
 import com.example.backend.exceptions.teams.TeamCreationException;
 import com.example.backend.exceptions.teams.TeamNotFoundException;
 import com.example.backend.exceptions.teams.TooManyTeamsException;
@@ -20,7 +15,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Optional;
 import java.util.Set;
 import java.util.UUID;
 
@@ -46,32 +40,6 @@ public class TeamController {
     public ResponseEntity<Team> createTeam(@RequestBody CreateTeamDTO createTeamDTO, @AuthenticationPrincipal User user) throws TeamCreationException, UserNotFoundException {
         Team team = teamService.createTeam(createTeamDTO, user);
         return ResponseEntity.ok(team);
-    }
-
-    @GetMapping("/{teamId}/boards")
-    public ResponseEntity<Set<Board>> getTeamBoards(@PathVariable UUID teamId) {
-        Set<Board> boards = boardService.getTeamBoards(teamId);
-        return ResponseEntity.ok(boards);
-    }
-
-    @GetMapping("/boards/{boardId}")
-    public ResponseEntity<Board> getBoardById(@PathVariable UUID boardId) {
-        Optional<Board> board = boardService.getBoardById(boardId);
-        return board.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
-
-    }
-
-    @PutMapping("/boards/{boardId}")
-    public ResponseEntity<Board> updateBoard(@PathVariable UUID boardId, @RequestBody UpdateBoardDTO updateBoardDTO) throws BoardNotFoundException {
-        Board board = boardService.updateBoard(boardId, updateBoardDTO);
-        return ResponseEntity.ok(board);
-    }
-
-    @PostMapping("/{teamId}/boards")
-    public ResponseEntity<Board> createBoard(@PathVariable UUID teamId, @RequestBody CreateBoardDTO dto) throws TooManyBoardsException, TeamNotFoundException {
-        dto.setTeamId(teamId);
-        Board board = boardService.createBoard(dto);
-        return ResponseEntity.status(HttpStatus.CREATED).body(board);
     }
 
     @PostMapping("/{teamId}/invite")
