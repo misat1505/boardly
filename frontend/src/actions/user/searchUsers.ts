6fe660b64f163a8fd4bf63f5e5aa291c9813fae6
misat1505 <misat2002@gmail.com>
2025-08-17 15:45:47 +0200

    resolved bug, updated .env.example

--- frontend/src/actions/user/searchUsers.ts
+++ frontend/src/actions/user/searchUsers.ts
@@ -4,7 +4,7 @@ import { User } from "@/types/User";
 import { api } from "../base";
 
 export async function searchUsers(searchString: string): Promise<User[]> {
-  const client = await api();
+  const client = await api({ attachAccessToken: true });
   const res = await client.get(`/users/search?q=${searchString}`);
 
   return res.data;
